812f45dbf49fb82ff90e5d6feee770401475f8f9
Rob Homan <homan.robertc@gmail.com>
2015-03-29 17:18:22 -0700

    Add initial plugin file, which adds theos-specific env variables and modifies the $PATH when applied.

---
+++ theos.plugin.zsh
@@ -0,0 +1,10 @@
+echo "Loading $0..."
+
+# Contains configuration settings and aliases for a typical
+# osx installation of the theos IDE
+
+export THEOS=/opt/theos
+export THEOS_DEVICE_IP=192.168.0.7 THEOS_DEVICE_PORT=22
+
+# Add cycript to the path
+export PATH=$PATH:/usr/local/Cycript_0.9.502
